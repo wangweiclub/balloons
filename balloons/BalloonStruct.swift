340832cd6d5f4d5d94a7d41cea9d297b9554f477
ob_club <ob_club@ob-clubdeMacBook-Pro.local>
2014-10-08 23:10:10 +0800

    first project

---
+++ balloons/BalloonStruct.swift
@@ -0,0 +1,15 @@
+//
+//  BalloonStruct.swift
+//  balloons
+//
+//  Created by ob_club on 14-10-8.
+//  Copyright (c) 2014年 wangweiclub. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Ballon{
+    var number=0
+    var image=UIImage(named:"")
+}
--- balloons/ViewController.swift
+++ balloons/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LableBallons: UILabel!
+    @IBOutlet weak var ImageBalloons: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ButtonBalloons(sender: UIBarButtonItem) {
+        
+    }
 
 }
 
